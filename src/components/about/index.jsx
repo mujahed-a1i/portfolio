ac3d6ef9e3b416f290c2c8051193c38a006383b9
Mujahed Ali <mujahed97@gmail.com>
2024-03-04 23:44:17 -0500

    add profile image to mobile

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -5,7 +5,8 @@ import Experience from "./Experience";
 import PersonalInfo from "./PersonalInfo";
 import Skills from "./Skills";
 import cv from "../../assets/img/cv.webp";
-import heroImgMobile from "../../assets/img/hero/img-mobile.jpg";
+import heroImgMobile from "../../assets/img/hero/profile.JPG";
+// import heroImgMobile from "../../assets/img/hero/img-mobile.jpg";
 
 const index = () => {
   return (
